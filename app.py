13391f681feb64c9bd7f5ec37e709c7d5100194a
wielomianik <wielomianowo@gmail.com>
2022-03-24 13:14:10 +0100

    File server hosting v1.2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_from_directory
-import glob, os
+import glob, os, inflect
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'files'
@@ -7,7 +7,9 @@ app.config['UPLOAD_FOLDER'] = 'files'
 @app.route("/")
 def home():
     filesPaths = glob.glob("files/*")
-    return render_template('home.html', filesPaths=filesPaths)
+    pathsIters = [inflect.engine().number_to_words(int(i[0])) for i in enumerate(filesPaths)]
+    filesPathsDict = dict(zip(pathsIters, filesPaths))
+    return render_template('home.html', filesPaths=filesPathsDict)
 
 @app.route('/files/<filename>', methods=['GET', 'POST'])
 def download(filename):
